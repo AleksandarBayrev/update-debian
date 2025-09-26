5a789b7d2f96cb8f42e2ee929f8a2572abeeae1a
Aleksandar Bayrev <aleksandarbayrev@gmail.com>
2025-09-26 18:59:17 +0300

    Update args parser

--- Services/ArgsParser.cs
+++ Services/ArgsParser.cs
@@ -4,11 +4,20 @@ namespace UpdateDebian.Services
 {
     public class ArgsParser : IArgsParser
     {
+        private static Exception GetErrorMessage()
+        {
+            return new Exception($"No action specified, valid actions are:{Environment.NewLine}{string.Join(Environment.NewLine, Actions.ValidActions)}");
+        }
         public string GetAction(string[] args)
         {
             if (args.Length != 1)
             {
-                throw new Exception($"No action specified, valid actions are:{Environment.NewLine}{string.Join(Environment.NewLine, Actions.ValidActions)}");
+                throw GetErrorMessage();
+            }
+
+            if (args[0] is null || !Actions.ValidActions.Contains(args[0]))
+            {
+                throw GetErrorMessage();
             }
 
             return args[0];
